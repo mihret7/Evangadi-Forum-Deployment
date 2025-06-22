3f12fd09062aeaec293de86a75a1adfe605ae66e
Mihret7 <mihretbizuayehu777@gmail.com>
2025-06-22 17:48:17 +0300

    test

--- server/db/db.Config.js
+++ server/db/db.Config.js
@@ -10,6 +10,9 @@ const dbconnection = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   max: process.env.DB_CONNECTION_LIMIT || 10,
+  ssl: {
+    rejectUnauthorized: false, // Render uses trusted certs so this is safe for most cases
+  },
 });
 
 module.exports = dbconnection;
