143c7dc5c3153b8473d5b7f1c9c5004bb89144cd
Mihret7 <mihretbizuayehu777@gmail.com>
2025-06-22 16:05:21 +0300

    test

--- server/app.js
+++ server/app.js
@@ -10,7 +10,13 @@ dotenv.config();
 const app = express();
 
 // middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://evangadi-forum-mihret.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
