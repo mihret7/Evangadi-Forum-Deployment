ac4de996edd14897c407fb6aa7607f608a19d845
Mihret7 <mihretbizuayehu777@gmail.com>
2025-06-22 17:55:08 +0300

    test

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: "https://evangadi-forum-mihret.vercel.app",
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
