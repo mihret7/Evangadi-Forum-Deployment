e961d678c43b01ccbd13e0a2ae2b64f806106d1e
Mihret7 <mihretbizuayehu777@gmail.com>
2025-06-22 18:23:03 +0300

    postgreSQL

---
+++ server/controller/loadDummyDataController.js
@@ -0,0 +1,19 @@
+const dbconnection = require("../db/db.Config");
+const dummy_data = require("../db/dummy_data.js");
+const fs = require("fs");
+
+
+async function insertDummyData() {
+  const sql = fs.readFileSync(dummy_data, "utf8");
+  try {
+    await dbconnection.query(sql);
+    console.log("Dummy data inserted successfully");
+  } catch (err) {
+    console.error("Error inserting dummy data:", err);
+  } finally {
+    await dbconnection.end();
+  }
+}
+
+module.exports = {
+  insertDummyData, };
